d67885b6e453140bff569962b55b50e606824c79
Pratush <pratushbarua26@gmail.com>
2025-04-09 14:18:04 +0600

    initially

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: [
+        'images.pexels.com',           // Existing allowed domain
+        'maps.googleapis.com'          // Allow Google Static Maps
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
