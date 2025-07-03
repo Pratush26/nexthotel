9ab7c2e6d3e91351784b3e3829fc2217c55502bb
Pratush <pratushbarua26@gmail.com>
2025-07-03 12:11:16 +0600

    mid fix

--- middleware.js
+++ middleware.js
@@ -3,14 +3,14 @@ import { auth } from "@/auth.config";
 import { NextResponse } from "next/server";
 
 export default auth((req) => {
+  const { pathname } = req.nextUrl;
   if (!req.auth) {
     return NextResponse.rewrite(new URL("/not-found", req.url)); // req.nextUrl - /cage/orders req.url - http://localhost:3000/cage/orders
   }
   
-  if (req.nextUrl.startsWith("/cage/admin") && req.auth?.user.role !== "admin") {
+  if (pathname.startsWith("/cage/admin") && req.auth?.user.role !== "admin") {
     return NextResponse.rewrite(new URL("/not-found", req.url));
   }
-
   return NextResponse.next();
 });
 
